d2498f183bd40efff04395a3cc638a7acbc8c949
ajmalpk23 <ajmalpk23@gmail.com>
2021-06-16 14:11:54 +0530

    inital fetching completed

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect,useCallback} from 'react';
 import {
   View,
   Text,
@@ -6,7 +6,7 @@ import {
   Image,
   TouchableOpacity,
   ImageBackground,
-  ScrollView,
+  ScrollView, RefreshControl
 } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
@@ -14,21 +14,72 @@ import AppBar from '../config/appbar';
 
 import Story from '../config/storys';
 import Modal from 'react-native-modal';
-
-
-
+import firestore from '@react-native-firebase/firestore';
 
 function Home({navigation}) {
+  const [refreshing, setRefreshing] = useState(false);
+  const [Data, setdata] = useState([]);
+  const [id, setid] = useState();
+  const list = [];
+
+  const Show = () =>{
+    firestore()
+    .collection('Post')
+    .get()
+    .then(querySnapshot => {
+      console.log('Total users: ', querySnapshot.size);
+      // console.log(querySnapshot)
+
+      const Posts = querySnapshot.docs.map(pro => {
+        return {
+          ...pro.data(),
+          id: pro.id,
+        };
+      });
+      setdata(Posts);
+
+     
+    });
+  }
+  useEffect(() => {
+    
+    Show()
+   
+  },[]);
+
   const [isModalVisible, setModalVisible] = useState(false);
   const toggleModal = () => {
     setModalVisible(!isModalVisible);
   };
+  const wait = (timeout) => {
+    return new Promise(resolve => setTimeout(resolve, timeout));
+  }
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    Show()
+
+    wait(2000).then(() => setRefreshing(false));
+  }, []);
 
   return (
     <View>
       <AppBar />
       <ScrollView
-        style={{backgroundColor: '#bec3cc', width: '100%', paddingBottom: 62,marginBottom:60}}>
+        style={{
+          backgroundColor: '#bec3cc',
+          width: '100%',
+          paddingBottom: 62,
+          marginBottom: 60,
+        }}
+        
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+          />
+        }
+        >
         <View style={{width: '100%', backgroundColor: '#fff', height: 120}}>
           <Text
             style={{color: '#000', fontWeight: 'bold', fontSize: 18, left: 15}}>
@@ -38,175 +89,71 @@ function Home({navigation}) {
             <Story />
           </ScrollView>
         </View>
-        <TouchableOpacity onPress={() => navigation.navigate('POstDetail')}
-          style={{
-            width: '92%',
-            left: '4%',
-            right: '4%',
-            backgroundColor: '#fff',
-            borderRadius: 15,
-            // top: 10,
-            paddingBottom: 20,
-            marginTop:10
-          }}>
-          <View style={{flexDirection: 'row', left: 10, top: 10}}>
-            <Image
-              source={require('../images/user1.jpg')}
-              style={{height: 40, width: 40, borderRadius: 50}}
-            />
-            <View>
-              <Text style={{fontWeight: 'bold', left: 6, top: 2}}>
-                muhammed ajmal pk
-              </Text>
-              <Text style={{fontSize: 10, left: 6}}>Newyork</Text>
-            </View>
-          </View>
-          <Image
-            source={require('../images/p7.jpg')}
-            style={{
-              height: 200,
-              width: '90%',
-              alignSelf: 'center',
-              top: 20,
-              borderRadius: 15,
-            }}
-          />
-          <Text style={{fontWeight: 'bold', fontSize: 14, left: 15, top: 20,justifyContent:'center',flex:1,marginRight:25 }}>
-            {' '}
-            a long established fact that a reader will be distracted by the
+
+      <View style={{marginBottom:90}}>
+      {Data.map(rs => {
+          return (
+            <TouchableOpacity
+              onPress={() => navigation.navigate('POstDetail')}
+              style={{
+                width: '92%',
+                left: '4%',
+                right: '4%',
+                backgroundColor: '#fff',
+                borderRadius: 15,
+                // top: 10,
+                paddingBottom: 20,
+                marginTop: 10,
+                marginBottom:0
+              }}>
+              <View style={{flexDirection: 'row', left: 10, top: 10}}>
+                <Image
+                  source={require('../images/user1.jpg')}
+                  style={{height: 40, width: 40, borderRadius: 50}}
+                />
+                <View>
+                  <Text style={{fontWeight: 'bold', left: 6, top: 2}}>
+                    muhammed ajmal pk
+                  </Text>
+                  <Text style={{fontSize: 10, left: 6}}>Newyork</Text>
+                </View>
+              </View>
+              <Image
+                source={{uri:rs.url}}
+                style={{
+                  height: 200,
+                  width: '90%',
+                  alignSelf: 'center',
+                  top: 20,
+                  borderRadius: 15,
+                }}
+              />
+              <Text
+                style={{
+                  fontWeight: 'bold',
+                  fontSize: 14,
+                  left: 15,
+                  top: 20,
+                  justifyContent: 'center',
+                  flex: 1,
+                  marginRight: 25,
+                }}>
+                {' '}
+                {rs.Subject}
+                {/* a long established fact that a reader will be distracted by the
             readable content of a page when looking at its layout. The point of
             using Lorem Ipsum is that it has a more-or-less normal distribution
-            of letters, a
-          </Text>
-          <Text style={{fontSize: 10, paddingBottom: 20, top: 20,left:15}}> DAte</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate('POstDetail')}
-          style={{
-            width: '92%',
-            left: '4%',
-            right: '4%',
-            backgroundColor: '#fff',
-            borderRadius: 15,
-            // top: 10,
-            paddingBottom: 20,
-            marginTop:10
-          }}>
-          <View style={{flexDirection: 'row', left: 10, top: 10}}>
-            <Image
-              source={require('../images/user1.jpg')}
-              style={{height: 40, width: 40, borderRadius: 50}}
-            />
-            <View>
-              <Text style={{fontWeight: 'bold', left: 6, top: 2}}>
-                muhammed ajmal pk
+            of letters, a */}
               </Text>
-              <Text style={{fontSize: 10, left: 6}}>Newyork</Text>
-            </View>
-          </View>
-          <Image
-            source={require('../images/p7.jpg')}
-            style={{
-              height: 200,
-              width: '90%',
-              alignSelf: 'center',
-              top: 20,
-              borderRadius: 15,
-            }}
-          />
-          <Text style={{fontWeight: 'bold', fontSize: 14, left: 15, top: 20,justifyContent:'center',flex:1,marginRight:25 }}>
-            {' '}
-            a long established fact that a reader will be distracted by the
-            readable content of a page when looking at its layout. The point of
-            using Lorem Ipsum is that it has a more-or-less normal distribution
-            of letters, a
-          </Text>
-          <Text style={{fontSize: 10, paddingBottom: 20, top: 20,left:15}}> DAte</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate('POstDetail')}
-          style={{
-            width: '92%',
-            left: '4%',
-            right: '4%',
-            backgroundColor: '#fff',
-            borderRadius: 15,
-            // top: 10,
-            paddingBottom: 20,
-            marginTop:10
-          }}>
-          <View style={{flexDirection: 'row', left: 10, top: 10}}>
-            <Image
-              source={require('../images/user1.jpg')}
-              style={{height: 40, width: 40, borderRadius: 50}}
-            />
-            <View>
-              <Text style={{fontWeight: 'bold', left: 6, top: 2}}>
-                muhammed ajmal pk
+              <Text
+                style={{fontSize: 10, paddingBottom: 20, top: 20, left: 15}}>
+                {' '}
+                DAte
               </Text>
-              <Text style={{fontSize: 10, left: 6}}>Newyork</Text>
-            </View>
-          </View>
-          <Image
-            source={require('../images/p7.jpg')}
-            style={{
-              height: 200,
-              width: '90%',
-              alignSelf: 'center',
-              top: 20,
-              borderRadius: 15,
-            }}
-          />
-          <Text style={{fontWeight: 'bold', fontSize: 14, left: 15, top: 20,justifyContent:'center',flex:1,marginRight:25 }}>
-            {' '}
-            a long established fact that a reader will be distracted by the
-            readable content of a page when looking at its layout. The point of
-            using Lorem Ipsum is that it has a more-or-less normal distribution
-            of letters, a
-          </Text>
-          <Text style={{fontSize: 10, paddingBottom: 20, top: 20,left:15}}> DAte</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate('POstDetail')}
-          style={{
-            width: '92%',
-            left: '4%',
-            right: '4%',
-            backgroundColor: '#fff',
-            borderRadius: 15,
-            // top: 10,
-            paddingBottom: 20,
-            marginTop:10
-          }}>
-          <View style={{flexDirection: 'row', left: 10, top: 10}}>
-            <Image
-              source={require('../images/user1.jpg')}
-              style={{height: 40, width: 40, borderRadius: 50}}
-            />
-            <View>
-              <Text style={{fontWeight: 'bold', left: 6, top: 2}}>
-                muhammed ajmal pk
-              </Text>
-              <Text style={{fontSize: 10, left: 6}}>Newyork</Text>
-            </View>
-          </View>
-          <Image
-            source={require('../images/p7.jpg')}
-            style={{
-              height: 200,
-              width: '90%',
-              alignSelf: 'center',
-              top: 20,
-              borderRadius: 15,
-            }}
-          />
-          <Text style={{fontWeight: 'bold', fontSize: 14, left: 15, top: 20,justifyContent:'center',flex:1,marginRight:25 }}>
-            {' '}
-            a long established fact that a reader will be distracted by the
-            readable content of a page when looking at its layout. The point of
-            using Lorem Ipsum is that it has a more-or-less normal distribution
-            of letters, a
-          </Text>
-          <Text style={{fontSize: 10, paddingBottom: 20, top: 20,left:15}}> DAte</Text>
-        </TouchableOpacity>
-        
+            </TouchableOpacity>
+          );
+        })}
+      </View>
       </ScrollView>
     </View>
   );
